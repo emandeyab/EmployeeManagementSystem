b84851c1707db151937b17335f7c9328c8f1fb0b
Roaa Mohamed <roaaamohamed66@gmail.com>
2024-12-07 11:36:17 +0200

    modify database

--- Employee-management-system/Database/emp.sql
+++ Employee-management-system/Database/emp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 02, 2024 at 10:57 AM
+-- Generation Time: Dec 07, 2024 at 10:35 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -28,18 +28,19 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admin` (
-  `admin_id` int(12) NOT NULL,
+  `admin_id` int(11) NOT NULL,
+  `person_id` int(11) NOT NULL,
   `email` varchar(225) NOT NULL,
-  `password` varchar(225) NOT NULL,
-  `person_id` int(12) NOT NULL
+  `password` varchar(225) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`admin_id`, `email`, `password`, `person_id`) VALUES
-(1, 'root1@gmail.com', 'root123', 1);
+INSERT INTO `admin` (`admin_id`, `person_id`, `email`, `password`) VALUES
+(1, 1, 'admin.mohammed@company.com', 'root123'),
+(4, 2, 'admin.nour@company.com ', '123');
 
 -- --------------------------------------------------------
 
@@ -48,11 +49,20 @@ INSERT INTO `admin` (`admin_id`, `email`, `password`, `person_id`) VALUES
 --
 
 CREATE TABLE `department` (
-  `department_id` int(12) NOT NULL,
-  `name` int(225) NOT NULL,
-  `location` int(225) NOT NULL,
+  `department_id` int(11) NOT NULL,
+  `manager_id` int(11) NOT NULL,
+  `name` varchar(225) NOT NULL,
+  `location` varchar(225) NOT NULL,
   `emps_number` int(12) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`department_id`, `manager_id`, `name`, `location`, `emps_number`) VALUES
+(5, 2, 'Sales', 'Alex', 1),
+(101, 1, 'Development', 'Alex', 1);
 
 -- --------------------------------------------------------
 
@@ -61,12 +71,20 @@ CREATE TABLE `department` (
 --
 
 CREATE TABLE `employee` (
-  `employee_id` int(12) NOT NULL,
+  `employee_id` int(11) NOT NULL,
+  `person_id` int(11) NOT NULL,
+  `department_id` int(11) NOT NULL,
   `logi_id` int(14) NOT NULL,
-  `password` varchar(225) NOT NULL,
-  `department_id` int(12) NOT NULL,
-  `task_id` int(12) DEFAULT NULL
+  `password` varchar(225) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`employee_id`, `person_id`, `department_id`, `logi_id`, `password`) VALUES
+(20, 100, 101, 2034, '123'),
+(21, 101, 5, 2000, '1234');
 
 -- --------------------------------------------------------
 
@@ -75,11 +93,19 @@ CREATE TABLE `employee` (
 --
 
 CREATE TABLE `manager` (
-  `manager_id` int(12) NOT NULL,
-  `email` int(225) NOT NULL,
-  `password` int(225) NOT NULL,
-  `department_id` int(12) NOT NULL
+  `manager_id` int(11) NOT NULL,
+  `person_id` int(11) NOT NULL,
+  `email` varchar(225) NOT NULL,
+  `password` varchar(225) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `manager`
+--
+
+INSERT INTO `manager` (`manager_id`, `person_id`, `email`, `password`) VALUES
+(1, 11, 'Development.manager.hamza@company.com', '9843hewnfmd02k'),
+(2, 10, 'Sales.manager.ghali@company.com', '1234');
 
 -- --------------------------------------------------------
 
@@ -88,7 +114,7 @@ CREATE TABLE `manager` (
 --
 
 CREATE TABLE `person` (
-  `person_id` int(12) NOT NULL,
+  `person_id` int(11) NOT NULL,
   `first_name` varchar(225) NOT NULL,
   `last_name` varchar(225) NOT NULL,
   `age` int(10) NOT NULL,
@@ -107,7 +133,60 @@ CREATE TABLE `person` (
 --
 
 INSERT INTO `person` (`person_id`, `first_name`, `last_name`, `age`, `address`, `phone_number`, `date_of_birth`, `absent_times`, `salary`, `job_title`, `gender`, `role`) VALUES
-(1, 'Mohamed', 'Ahmed', 30, 'menofia', '01012351246', '1996-12-09', NULL, 15300, 'root', 'male', 'admin');
+(1, 'Mohamed', 'Ahmed', 30, 'Cairo', '01012351246', '1996-12-09', NULL, 16600, 'root', 'male', 'admin'),
+(2, 'Nour', 'Mohyee', 28, 'Cairo', '9387450823', '1996-02-04', 0, 30000, 'root', 'female', 'admin'),
+(10, 'Ghali', 'Ali', 29, 'El-Maadi', '34567899876', '1995-02-04', 0, 30000, 'Team leads', 'male', 'manager'),
+(11, 'Hamza', 'Namira', 30, 'UAE', '085329-12', '1994-02-04', 6, 33000, 'Direct supervisor', 'male', 'manager'),
+(100, 'Ola', 'Sarhan', 25, 'Kafr-Tenbady', '3408508', '2000-01-01', 0, 10000, 'Web Designer', 'female', 'employee'),
+(101, 'Samira', 'Samer', 22, 'Abo El-Gheet', '32083048532', '2003-01-01', 0, 10000, 'Accountant', 'female', 'employee');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `task_id` int(11) NOT NULL,
+  `employee_id` int(11) NOT NULL,
+  `description` varchar(50) DEFAULT NULL,
+  `status` enum('To Do','Done','In Progress') DEFAULT NULL,
+  `start_date` date DEFAULT NULL,
+  `end_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `tasks`
+--
+
+INSERT INTO `tasks` (`task_id`, `employee_id`, `description`, `status`, `start_date`, `end_date`) VALUES
+(1, 20, 'create CRUD table for managers', 'Done', '2024-12-05', '2024-12-18'),
+(2, 21, 'Create use case description', 'In Progress', '2024-12-04', '2024-12-31');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vacations`
+--
+
+CREATE TABLE `vacations` (
+  `vacation_id` int(11) NOT NULL,
+  `employee_id` int(11) NOT NULL,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
+  `causes` varchar(50) DEFAULT NULL,
+  `status` enum('Pending','Approved','Rejected') DEFAULT NULL,
+  `department_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `vacations`
+--
+
+INSERT INTO `vacations` (`vacation_id`, `employee_id`, `start_date`, `end_date`, `causes`, `status`, `department_id`) VALUES
+(12, 20, '2024-12-07', '2024-12-09', 'Sick Leave', 'Pending', 101),
+(13, 20, '2024-12-04', '2024-12-05', 'Emergency Leave', 'Pending', 101),
+(14, 21, '2024-12-04', '2024-12-05', 'Sick leave', 'Pending', 5);
 
 --
 -- Indexes for dumped tables
@@ -118,31 +197,49 @@ INSERT INTO `person` (`person_id`, `first_name`, `last_name`, `age`, `address`, 
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`admin_id`),
-  ADD KEY `admin_idfk` (`person_id`);
+  ADD KEY `person_id` (`person_id`);
 
 --
 -- Indexes for table `department`
 --
 ALTER TABLE `department`
-  ADD PRIMARY KEY (`department_id`);
+  ADD PRIMARY KEY (`department_id`),
+  ADD KEY `manager_id` (`manager_id`);
 
 --
 -- Indexes for table `employee`
 --
 ALTER TABLE `employee`
-  ADD PRIMARY KEY (`employee_id`);
+  ADD PRIMARY KEY (`employee_id`),
+  ADD KEY `department_id` (`department_id`),
+  ADD KEY `person_id` (`person_id`);
 
 --
 -- Indexes for table `manager`
 --
 ALTER TABLE `manager`
-  ADD PRIMARY KEY (`manager_id`);
+  ADD PRIMARY KEY (`manager_id`),
+  ADD KEY `person_id` (`person_id`);
 
 --
 -- Indexes for table `person`
 --
 ALTER TABLE `person`
   ADD PRIMARY KEY (`person_id`);
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`task_id`),
+  ADD KEY `employee_id` (`employee_id`);
+
+--
+-- Indexes for table `vacations`
+--
+ALTER TABLE `vacations`
+  ADD PRIMARY KEY (`vacation_id`),
+  ADD KEY `employee_id` (`employee_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -152,31 +249,43 @@ ALTER TABLE `person`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `admin_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `department`
 --
 ALTER TABLE `department`
-  MODIFY `department_id` int(12) NOT NULL AUTO_INCREMENT;
+  MODIFY `department_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=102;
 
 --
 -- AUTO_INCREMENT for table `employee`
 --
 ALTER TABLE `employee`
-  MODIFY `employee_id` int(12) NOT NULL AUTO_INCREMENT;
+  MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `manager`
 --
 ALTER TABLE `manager`
-  MODIFY `manager_id` int(12) NOT NULL AUTO_INCREMENT;
+  MODIFY `manager_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `person`
 --
 ALTER TABLE `person`
-  MODIFY `person_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `person_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=102;
+
+--
+-- AUTO_INCREMENT for table `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `vacations`
+--
+ALTER TABLE `vacations`
+  MODIFY `vacation_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Constraints for dumped tables
@@ -186,14 +295,38 @@ ALTER TABLE `person`
 -- Constraints for table `admin`
 --
 ALTER TABLE `admin`
-  ADD CONSTRAINT `admin_idfk` FOREIGN KEY (`person_id`) REFERENCES `admin` (`admin_id`);
+  ADD CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `person` (`person_id`);
 
 --
 -- Constraints for table `department`
 --
 ALTER TABLE `department`
-  ADD CONSTRAINT `emp_dpt_idfk` FOREIGN KEY (`department_id`) REFERENCES `employee` (`employee_id`),
-  ADD CONSTRAINT `man_dpt_idfk` FOREIGN KEY (`department_id`) REFERENCES `manager` (`manager_id`);
+  ADD CONSTRAINT `department_ibfk_1` FOREIGN KEY (`manager_id`) REFERENCES `manager` (`manager_id`);
+
+--
+-- Constraints for table `employee`
+--
+ALTER TABLE `employee`
+  ADD CONSTRAINT `employee_ibfk_1` FOREIGN KEY (`department_id`) REFERENCES `department` (`department_id`),
+  ADD CONSTRAINT `employee_ibfk_2` FOREIGN KEY (`person_id`) REFERENCES `person` (`person_id`);
+
+--
+-- Constraints for table `manager`
+--
+ALTER TABLE `manager`
+  ADD CONSTRAINT `manager_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `person` (`person_id`);
+
+--
+-- Constraints for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`employee_id`);
+
+--
+-- Constraints for table `vacations`
+--
+ALTER TABLE `vacations`
+  ADD CONSTRAINT `vacations_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`employee_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
